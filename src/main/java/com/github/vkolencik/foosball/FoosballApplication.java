7ab0eb922e7ffe21a093006241acfe4b2f4f2d87
Vojtěch Kolenčík <vojtech.kolencik@e-invent.eu>
2019-01-20 15:01:58 +0100

    Reformat indentation according to EC/linting rules

--- src/main/java/com/github/vkolencik/foosball/FoosballApplication.java
+++ src/main/java/com/github/vkolencik/foosball/FoosballApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FoosballApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FoosballApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FoosballApplication.class, args);
+    }
 
 }
 
