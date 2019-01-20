985807a33f8f17f992e15c853b682795ea5aafae
Vojtěch Kolenčík <vojtech.kolencik@e-invent.eu>
2019-01-20 14:36:12 +0100

    Initial project structure
    * Basic Spring Boot structure generated by Initializr
    * .editorconfig
    * .gitignore
    * Basic DB settings, driver dependency
    * Readme scaffolding

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Foosball demo application
+
+Demo REST web app for _mall.cz_. Requires JDK 11.
+
+## Quick start
+
+
+## API
---
+++ src/main/java/com/github/vkolencik/foosball/FoosballApplication.java
@@ -0,0 +1,14 @@
+package com.github.vkolencik.foosball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoosballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoosballApplication.class, args);
+	}
+
+}
+
