28c9dd3182cdafcb9f79815a22cc35804be2b88b
Vojtěch Kolenčík <vojtech.kolencik@e-invent.eu>
2019-01-23 15:27:02 +0100

    Add players resource with custom ordering

--- src/main/java/com/github/vkolencik/foosball/controller/PlayerController.java
+++ src/main/java/com/github/vkolencik/foosball/controller/PlayerController.java
@@ -1,13 +1,18 @@
 package com.github.vkolencik.foosball.controller;
 
 import com.github.vkolencik.foosball.dto.PlayerDto;
+import com.github.vkolencik.foosball.dto.PlayerOrder;
 import com.github.vkolencik.foosball.service.PlayerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/players")
@@ -17,6 +22,23 @@ public class PlayerController {
 
     public PlayerController(PlayerService playerService) {
         this.playerService = playerService;
+    }
+
+    @GetMapping
+    public List<PlayerDto> getPlayers(
+        @RequestParam(value = "order-by", defaultValue = "wins") String orderBy,
+        @RequestParam Map<String, String> queryMap) {
+
+        PlayerOrder order;
+        try {
+            order = PlayerOrder.valueOf(orderBy.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                String.format("Unknown order: \"%s\"", orderBy));
+        }
+
+        return playerService.getPlayers(order, queryMap.containsKey("asc"));
     }
 
     @GetMapping("/{nickname}")
